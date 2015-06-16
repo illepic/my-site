98040eb0a6cb5195e3e0c47b9ead9eb92ea0f88c
Evan Lovely <evan@evanlovely.com>
2015-06-15 21:49:37 -0700

    pattern lab starter install

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
+# Pull gems from RubyGems
 source 'https://rubygems.org'
 
+# ~> Refers to all versions of the given gem on the current full version number, so it will be able to use any version of Compass Aurora until Compass Aurora 4.x.x. For a specific version of a gem, remove the ~>
 gem 'jekyll', '2.5.3'
+gem 'scss-lint'
+
---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function (grunt) {
+  "use strict";
+  var _ = require("lodash");
+  var config = grunt.file.readYAML("Gruntconfig.yml");
+  if (grunt.file.exists("Gruntconfig--custom.yml")) {
+    var customConfigOverrides = grunt.file.readYAML("Gruntconfig--custom.yml");
+    _.extend(config, customConfigOverrides);
+  }
+
+  // Begin Config
+
+  // Begin Modular Config
+  require('./grunt-tasks/pattern-lab/pattern-lab.js')(grunt, config);
+  //require('./grunt-tasks/compass/compass.js')(grunt, config);
+  require('./grunt-tasks/libsass/libsass.js')(grunt, config);
+  require('./grunt-tasks/jshint/jshint.js')(grunt, config);
+  //require('./grunt-tasks/drupal7/drupal7.js')(grunt, config);
+  require('./grunt-tasks/icons/icons.js')(grunt, config);
+  require('./grunt-tasks/regression-qa/regression-qa.js')(grunt, config);
+  // End Modular Config
+
+  // Begin Misc Config
+  grunt.config.merge({
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      dev: {
+        tasks: [
+          "watch",
+          "connect"
+        ]
+      }
+    },
+    bump: {// https://github.com/vojtajina/grunt-bump
+      options: {
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['-a'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+      }
+    }
+  });
+  // End Misc Config
+
+
+// End Config
+
+// Begin Task Aliases
+  grunt.registerTask("compile", [
+    "plBuild",
+    "icons-build",
+    "pattern_lab_component_builder",
+    "stylesCompile",
+    "shell:plBuild",
+    "shell:livereload"
+  ]);
+  grunt.registerTask("build", "compile");
+
+  grunt.registerTask("validate", [
+    "jsonlint",
+    "jshint",
+    "scsslint"
+  ]);
+
+  // this is ran if you do either `grunt default` or `grunt`
+  grunt.registerTask("default", [
+    "compile",
+    "concurrent:dev"
+  ]);
+// End Task Aliases
+
+  require('time-grunt')(grunt); // shows how long grunt tasks take ~ https://github.com/sindresorhus/time-grunt
+  require("load-grunt-tasks")(grunt);
+
+};
