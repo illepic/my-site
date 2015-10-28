d0247fb3be4e92c45259ed745b1e709cc0320bdd
Evan Lovely <evan@evanlovely.com>
2015-10-27 18:29:38 -0700

    setting up metalsmith branching ability

--- metalsmith.js
+++ metalsmith.js
@@ -7,51 +7,45 @@ var templates = require('metalsmith-react-templates');
 var define = require('metalsmith-define');
 var fileMetadata = require('metalsmith-filemetadata');
 var dateInFile = require('metalsmith-date-in-filename');
+var branch = require('metalsmith-branch');
 
 var metalsmith = Metalsmith(__dirname)
   .source('./source/_posts')
   //.source('./tests')
   .clean(false)
   .destination('./public')
-  //.use(function(files, metalsmith, done) {
-  //    var metadata = metalsmith.metadata();
-  //    metadata.rtemplate = 'post.jsx';
-  //  done();
-  //})
-  .use(dateInFile())
-  .use(markdown())
-  //.use(permalinks({
-  //  pattern: ':date/:title',
-  //  date: 'YYYY'
-  //}))
-  .use(fileMetadata([
-    {
-      pattern: '**/*.{md,html}',
-      preserve: true,
-      metadata: {
-        section: 'blog',
-        rtemplate: 'post.jsx'
+  .use(branch('**/*.md')
+    .use(markdown())
+  )
+  .use(branch('**/*.{md,html}')
+    .use(dateInFile())
+    //.use(permalinks({
+    //  pattern: ':date/:title',
+    //  date: 'YYYY'
+    //}))
+    .use(fileMetadata([
+      {
+        pattern: '**/*.{md,html}',
+        preserve: true,
+        metadata: {
+          section: 'blog',
+          rtemplate: 'post.jsx'
+        }
       }
-    }
-  ]))
-  //.use(function(files, metalsmith, done) {
+    ]))
+    //.use(function(files, metalsmith, done) {
     //console.log(files);
-  //  console.log(metalsmith.metadata());
-  //})
-  .use(templates({
-    isStatic: false,
-    directory: './src/templates',
-    requireIgnoreExt: ['.scss'],
-    babel: true
-    //baseFile: 'base.html'
-  }))
-  //.use(layouts({
-  //  engine: 'react',
-  //  default: 'page.jsx',
-  //  partials: './src',
-  //  directory: './src/base'
-  //}))
-  .build(function(err, files) {
+    //  console.log(metalsmith.metadata());
+    //})
+    .use(templates({
+      isStatic: false,
+      directory: './src/templates',
+      requireIgnoreExt: ['.scss'],
+      babel: true
+      //baseFile: 'base.html'
+    }))
+  )
+  .build(function (err, files) {
     if (err) {
       console.log('Error!');
       console.log(err);
--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -3,10 +3,14 @@ var Site = require('../global/site.jsx');
 
 var Page = React.createClass({
   render: function() {
+    var myDate = 'no date';
+    if (this.props.date) {
+      myDate = this.props.date.toString();
+    }
     return (
       <Site title={this.props.title} styleFiles={["/assets/style--post.css"]} scriptFiles={["/assets/bundle--post.js"]}>
         <h2>Post: {this.props.title}</h2>
-        <p>Date: {this.props.date.toString()}</p>
+        <p>Date: {myDate}</p>
         <div dangerouslySetInnerHTML={{__html: this.props.contents}}></div>
       </Site>
     );
