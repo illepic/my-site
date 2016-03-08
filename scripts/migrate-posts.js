aa22db09cdfadc21e90ee86b783b1044549abae3
Evan Lovely <evan@evanlovely.com>
2016-03-07 18:59:34 -0800

    migrating posts into date folders of year/month

--- scripts/migrate-posts.js
+++ scripts/migrate-posts.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 'use strict';
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const matter = require('gray-matter');
 
 let dir = './content/posts';
 
-fs.readdirSync(dir).forEach((file) => {
-  let fileData = matter.read(path.join(dir, file));
-  let contents = fileData.content.concat();
-  delete fileData.content;
-  let date = file.substring(0,10);
-  fileData.data.date = date;
-  fs.writeFileSync(path.join(dir, file), matter.stringify(contents, fileData.data));
+fs.readdirSync(dir).filter(item => !fs.statSync(path.join(dir,item)).isDirectory()).forEach((file) => {
+  //let fileData = matter.read(path.join(dir, file));
+  //let contents = fileData.content.concat();
+  //delete fileData.content;
+  let date = file.substring(0,11);
+  let year = file.substring(0,4);
+  let month = file.substring(5,7);
+  fs.ensureDirSync(path.join(dir, year, month));
+  fs.renameSync(path.join(dir, file), path.join(dir, year, month, file.replace(date, '')));
+  let x = true;
+  //fileData.data.date = date;
+  //fs.writeFileSync(path.join(dir, file), matter.stringify(contents, fileData.data));
 });
