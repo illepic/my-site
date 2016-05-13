9bf8ed42911f6e5f959337759f2aedd4507f8439
Evan Lovely <evan@evanlovely.com>
2016-05-12 18:37:27 -0700

    setting up defaultProps

--- test.jsx
+++ test.jsx
@@ -1,3 +1,15 @@
+"use strict";
 const React = require('react');
 
-module.exports = (props) => <div>Hello {props.title}!</div>;
+const Test = (props) => {
+  return (<div>
+    <h3>Hello {props.title}!</h3>
+    <h5>{props.subtitle}</h5>
+  </div>);
+};
+
+Test.defaultProps = {
+  subtitle: 'default subtitle'
+};
+
+module.exports = Test;
