c795b10439fc093ff6c525cb21e85df935dddb62
Evan Lovely <evan@evanlovely.com>
2016-06-10 16:39:37 -0700

    adding back in individual blog post template content

---
+++ src/templates/blog-post/blog-post.jsx
@@ -0,0 +1,26 @@
+'use strict';
+const React = require('react');
+const Default = require('../default/default');
+const Markdown = require('../../global/markdown');
+
+const BlogPost = (props) => {
+  let pager;
+  if (props.previous || props.next) {
+    pager = (<nav className="pager">
+      {props.previous ? (
+        <a href={ props.previous.path } className="pager__link pager__link--prev">Previous - {props.previous.title}</a>
+      ) : ''}
+      {props.next ? (
+        <a href={ props.next.path } className="pager__link pager__link--next">Next - {props.next.title}</a>
+      ) : ''}
+    </nav>);
+  }
+  return (
+    <Default {...props}>
+      <Markdown contents={props.contents}/>
+      {pager}
+    </Default>
+  );
+};
+
+module.exports = BlogPost;
