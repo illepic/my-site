5549d29f02928771dadb286a6bd9b1f37c53ed34
Evan Lovely <evan@evanlovely.com>
2016-06-10 15:06:10 -0700

    setting up basic cards in Styleguide

--- src/templates/styleguide/styleguide.jsx
+++ src/templates/styleguide/styleguide.jsx
@@ -2,12 +2,23 @@
 const React = require('react');
 const Default = require('../default/default');
 const Markdown = require('../../global/markdown');
+const Card = require('../../molecules/card/card');
+
+const Cards = (props) => {
+  return (<div>
+    <h4>Cards</h4>
+    <h5>Simple</h5>
+    <Card {...props.dummy} excerpt={false} />
+    <h5>With Excerpt</h5>
+    <Card {...props.dummy} />
+  </div>);
+};
 
 const Styleguide = (props) => {
   return (
     <Default {...props}>
       <Markdown contents={props.contents} />
-      
+      <Cards {...props} />
     </Default>
   );
 };
