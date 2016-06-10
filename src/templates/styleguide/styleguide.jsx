3f3dc4c06d91848ccf3ff2deda58fa8bf03ee67d
Evan Lovely <evan@evanlovely.com>
2016-06-10 15:30:20 -0700

    using example yaml files to add to styleguide

--- src/templates/styleguide/styleguide.jsx
+++ src/templates/styleguide/styleguide.jsx
@@ -1,23 +1,33 @@
 'use strict';
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+const glob = require('glob');
 const React = require('react');
 const Default = require('../default/default');
 const Markdown = require('../../global/markdown');
 const Card = require('../../molecules/card/card');
 
 const Cards = (props) => {
+  let allCards = glob.sync('src/molecules/card/examples/*.{yml,yaml}').map(example => {
+    let name = path.basename(example, path.extname(example));
+    let data = Object.assign({}, props.dummy, yaml.safeLoad(fs.readFileSync(example, 'utf8')));
+    return (<div>
+      <h5 style={{textTransform: 'capitalize'}}>{name}</h5>
+      <Card {...data} />
+    </div>);
+  });
   return (<div>
     <h4>Cards</h4>
-    <h5>Simple</h5>
-    <Card {...props.dummy} excerpt={false} />
-    <h5>With Excerpt</h5>
-    <Card {...props.dummy} />
+    {allCards}
+    <hr />
   </div>);
 };
 
 const Styleguide = (props) => {
   return (
     <Default {...props}>
-      <Markdown contents={props.contents} />
+      <Markdown contents={props.contents}/>
       <Cards {...props} />
     </Default>
   );
