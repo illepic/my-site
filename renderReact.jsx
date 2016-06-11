1569d37b4f7ac6b794720f34dfa72b4791a1aeed
Evan Lovely <evan@evanlovely.com>
2016-06-11 15:23:55 -0700

    Adding typeset for rich quotes and stuff

--- renderReact.jsx
+++ renderReact.jsx
@@ -1,9 +1,10 @@
 "use strict";
 const React = require('react');
 const ReactDomServer = require('react-dom/server');
+const typeset = require('typeset');
 
 module.exports = function(file, props) {
   let Template = require(file);
   props = props || {};
-  return ReactDomServer.renderToStaticMarkup(<Template {...props} />);
+  return typeset(ReactDomServer.renderToStaticMarkup(<Template {...props} />));
 };
