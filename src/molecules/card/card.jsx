453850716e52710896d02a7320261d0f68f57874
Evan Lovely <evan@evanlovely.com>
2016-06-11 15:31:51 -0700

    cleanup

--- src/molecules/card/card.jsx
+++ src/molecules/card/card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
   return (
     <article className="card">
       <h3 className="card__title"><a href={ props.path }>{ props.title }</a></h3>
-      {props.excerpt ? (<div className="card__excerpt">{ props.excerpt }</div>) : ''}
+      {props.excerpt ? (<div className="card__excerpt">{ props.excerpt}</div>) : ''}
     </article>
   );
 };
