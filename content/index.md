acab85bc3e8389626cb758aa9b4e96b77d4113d0
Evan Lovely <evan@evanlovely.com>
2016-08-06 22:59:08 -0700

    removing home page copy

--- content/index.md
+++ content/index.md
@@ -4,8 +4,3 @@ template: home
 hideToc: true
 hideRecentPosts: true
 ---
-## Hi! I’m Evan, and I theme Drupal websites with a responsive, mobile approach.
-
-I am a front end web developer & designer who crafts websites that are easy to use and powerful. A love of liquid and responsive site design makes sure your site looks good on big monitors and small mobile devices (try resizing your browser on this site!) Clients and teams love to work with me because of my friendly attitude and my contagious passion.
-
-**Thanks for stopping by!**
--- src/templates/home/home.jsx
+++ src/templates/home/home.jsx
@@ -1,25 +1,23 @@
 const React = require('react');
-const Link = require('../../atoms/link');
 const LandingList = require('../../organisms/landing-list/landing-list');
 
 const Home = (props) => (<div>
-  <p>
-    <Link className="button button--lg" href="/portfolio">Portfolio</Link>
-    <Link className="button" href="/contact">Contact</Link>
-  </p>
-    <LandingList
-      items={props.site.pages
-        .filter(page => page.section === 'blog' && !page.landingPage)
-        .sort((a, b) => new Date(b.date) - new Date(a.date))
-        .slice(0, 6)
-      }
-      showExcerpts={false}
-    />
+  <LandingList
+    items={props.site.pages
+      .filter(page => page.section === 'blog' && !page.landingPage)
+      .sort((a, b) => new Date(b.date) - new Date(a.date))
+      .slice(0, 6)
+    }
+    showExcerpts={false}
+  />
 </div>
 );
 
 Home.propTypes = {
   contents: React.PropTypes.string.isRequired,
+  site: React.PropTypes.shape({
+    pages: React.PropTypes.array,
+  }),
 };
 
 module.exports = Home;
