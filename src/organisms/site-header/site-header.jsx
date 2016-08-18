b04c08e6b2daac98327457bee94c44730818c864
Evan Lovely <evan@evanlovely.com>
2016-08-17 18:02:09 -0700

    better mobile menu

--- src/organisms/site-header/site-header.jsx
+++ src/organisms/site-header/site-header.jsx
@@ -4,32 +4,6 @@ const SiteNav = require('../../molecules/site-nav/site-nav');
 const Branding = require('../../molecules/branding');
 
 const SiteHeader = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      open: true,
-    };
-  }
-
-  componentWillMount() {
-    this.setState({
-      open: false,
-    });
-  }
-
-  componentWillReceiveProps() {
-    this.setState({
-      open: false,
-    });
-  }
-
-  toggle() {
-    this.setState({
-      open: !this.state.open,
-    });
-  }
-
   render() {
     let toc;
     if (!this.props.hideToc && this.props.toc.length !== 0) {
@@ -48,13 +22,9 @@ const SiteHeader = class extends React.Component {
       'site__header',
       'site-header',
     ];
-    if (this.state.open) classList.push('site__header--open');
     return (
       <header className={classList.join(' ')}>
         <Branding {...this.props} />
-        {/* <button className="site-header__nav-toggle" onClick={this.toggle}>Menu</button>*/}
-        {/* <label htmlFor="site-nav-toggle" className="site-header__nav-toggle">Menu</label>*/}
-        {/* <input type="checkbox" id="site-nav-toggle" className="site-header__nav-visible" />*/}
         <SiteNav pages={this.props.site.pages} currentPage={this.props.path} />
         {toc}
       </header>
